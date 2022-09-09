b0ff37b6aae858b930a5b78968988cde2826f666
Tobi Balogun <tobi.balo@gmail.com>
2022-09-09 08:54:20 +0100

    Add pages component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { TvshowsComponent } from './pages/tvshows/tvshows.component';
+import { GenresComponent } from './pages/genres/genres.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    MoviesComponent,
+    TvshowsComponent,
+    GenresComponent
   ],
   imports: [
     BrowserModule,
