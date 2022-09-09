ac519ccb2ee8b78042be9aa82494d24381796bec
Tobi Balogun <tobi.balo@gmail.com>
2022-09-09 08:56:20 +0100

    Add routes to pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GenresComponent } from './pages/genres/genres.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { TvshowsComponent } from './pages/tvshows/tvshows.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'movies',
+    component: MoviesComponent
+  },
+  {
+    path: 'tvshows',
+    component: TvshowsComponent
+  },
+  {
+    path: 'genres',
+    component: GenresComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
