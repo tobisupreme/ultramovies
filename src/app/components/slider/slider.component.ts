ef1a19fa14cccf297150abd42a2396e0eb6a9194
Tobi Balogun <tobi.balo@gmail.com>
2022-09-15 16:32:22 +0100

    style: animate slider component

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,17 +1,37 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 import { Movie } from '../../models/movie';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss']
+  styleUrls: ['./slider.component.scss'],
+  animations: [
+    trigger('slideFade', [
+      state('void', style({ opacity: 0 })),
+      transition('void => *', [animate('1s')]),
+      transition('* => void', [animate('500ms')]),
+    ]),
+  ],
 })
 export class SliderComponent implements OnInit {
+  // define input for slider component
   @Input() items: Movie[] = [];
 
-  constructor() { }
+  // setup slider slide timing
+  currentSlideIndex: number = 0;
+
+  constructor() {}
 
   ngOnInit(): void {
+    setInterval(() => {
+      return this.currentSlideIndex = ++this.currentSlideIndex % this.items.length;
+    }, 5000)
   }
-
 }
